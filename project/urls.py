e8e8bb9bb2c71a007f80a756475472867461e4b2
Jouhar kabeer <jouharkabeer412@gmail.com>
2024-03-03 11:03:43 +0530

    login pages

--- project/urls.py
+++ project/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    
+    #Admin Urls
+    path('admin_login/', views.admin_login, name='admin login' ),
+
+    #Student Urls
+    path('student_login/', views.student_login, name='student login' ),
+
+    #Faculty Urls
+    path('faculty_login/', views.faculty_login, name='faculty login' ),
+
+    #Alumni Urls
+    path('alumni_login/', views.alumni_login, name='alumni login' ),
 ]
--- project/views.py
+++ project/views.py
@@ -7,3 +7,15 @@ from datetime import date
 
 def index(request):
     return render(request, "index.html")
+
+def admin_login(request):
+    return render(request, "admin_login.html")
+
+def student_login(request):
+    return render(request, "student_login.html")
+
+def faculty_login(request):
+    return render(request, "faculty_login.html")
+
+def alumni_login(request):
+    return render(request, "alumni_login.html")
